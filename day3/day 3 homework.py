8176a788576a4415b63196d8a4887b01a1cdb171
zazusha <zazagugeshashvili1@gmail.com>
2024-03-05 01:10:30 +0400

    third commit

---
+++ day3/day 3 homework.py
@@ -0,0 +1,12 @@
+dads_name="shako"
+surname="gugeshashvili"
+print(dads_name,surname)
+
+moms_name="tatia"
+surname="jvarsheishvili"
+print(moms_name,surname)
+
+brother_name="cotne"
+surname="gugeshashvili"
+print(brother_name,surname)
+
