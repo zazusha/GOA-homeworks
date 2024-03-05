e0b847d26b274570c7ee87ac12afe68c04bbe8e3
zazusha <zazagugeshashvili1@gmail.com>
2024-03-05 15:40:17 +0400

    fourth commit

---
+++ day1/day 1 homework.py
@@ -0,0 +1,7 @@
+name="zaza"
+surname="gugeshashvili"
+age=19
+height=175
+weight="79kg"
+
+print("ჩემი სახელი და გვარია",name,surname,"ვარ",age,"წლის სიმაღლე",height,'სანტიმეტრი და წონა',weight,"კილოგრამი")
--- day3/day 3 homework.py
+++ day3/day 3 homework.py
@@ -1,12 +1,12 @@
 dads_name="shako"
-surname="gugeshashvili"
-print(dads_name,surname)
+surname1="gugeshashvili"
+print(dads_name,surname1)
 
 moms_name="tatia"
-surname="jvarsheishvili"
-print(moms_name,surname)
+surname2="jvarsheishvili"
+print(moms_name,surname2)
 
 brother_name="cotne"
-surname="gugeshashvili"
-print(brother_name,surname)
+surname3="gugeshashvili"
+print(brother_name,surname3)
 
