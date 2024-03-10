ee57988b996d6e1e276e9ccbb8e358d1aec86d10
zazusha <zazagugeshashvili1@gmail.com>
2024-03-10 18:03:25 +0400

    tenth commit

---
+++ day10/day9 main_homework.py
@@ -0,0 +1,102 @@
+
+#we must create 10 exaples to how to use if cycle
+#1 example
+
+
+# for i in range(20):
+#     print(i)
+
+
+#2 example
+
+
+# for i in range(0,100):
+#     print(i)
+
+
+#3 example
+
+
+# for i in range(0,100+1):
+#     print(i)
+
+
+#4 example
+
+
+# num=1
+
+# for i in range(10):
+#     num=num+i
+#     print(i)
+#     i=0
+
+
+#5 example
+    
+
+# i=0
+
+# for i in range(20):
+#     i=i+1
+#     print(i)
+
+
+#6 example
+
+
+# i=1
+
+# if i in range(20):
+#     i=i+1
+#     print(i)
+
+
+#7 example  
+    
+
+
+# num=0
+
+# for i in range(1,10):
+#     num=num+i
+    
+# print(num)   
+
+
+#8 example
+
+
+
+# for i in range(0,20+1):
+#     print(i)
+
+
+#9 example
+
+
+# num=2
+# for i in range(1,10):
+#     num=num+i
+# print(num)
+
+
+#10 example
+
+
+i=0
+num=2
+
+for i in range(10):
+    i=i+num
+print(i)
+
+
+
+
+
+
+
+
+
+
