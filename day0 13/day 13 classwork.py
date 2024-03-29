62883e2c4eb30f45668bf594b38aab063006cf68
zazusha <zazagugeshashvili1@gmail.com>
2024-03-29 21:49:27 +0400

    fifteenth commit

--- day0 13/day 13 classwork.py
+++ day0 13/day 13 classwork.py
@@ -9,24 +9,24 @@ else:
 
 #first method is working but we must use "and" code i guess )
 
-# age=int(input("tell me your age: "))
+age=int(input("tell me your age: "))
 
-# if age>0 and age<18:
-#     print("you are young")
-# elif age>=18 and age <50:
-#     print("you are old") 
-# else:
-#     print("you are too old")
+if age>0 and age<18:
+    print("you are young")
+elif age>=18 and age <50:
+    print("you are old") 
+else:
+    print("you are too old")
         
    
 
 
-# num=int(input("tell me number: "))
+num=int(input("tell me number: "))
 
-# if num > 0:
-#     print("this number is positive")
-# elif num <0:
-#     print("number is negative")
-# else:
-#     print("number equals 0")        
+if num > 0:
+    print("this number is positive")
+elif num <0:
+    print("number is negative")
+else:
+    print("number equals 0")        
 
