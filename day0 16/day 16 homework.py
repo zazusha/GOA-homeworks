25f94b42d13290c95a56187cfaba02816718ce20
zazusha <zazagugeshashvili1@gmail.com>
2024-04-01 23:51:56 +0400

    sixtheenth commit

---
+++ day0 16/day 16 homework.py
@@ -0,0 +1,39 @@
+#les gooo i am back, we are starting first  homework
+
+
+fav_films=["fight club","interstellar","green mile","oppenheimer"]
+
+print(fav_films)
+print(fav_films[0])
+
+
+#second
+
+
+names=['zaza','nika','luka','ana','emzari','anrlondi','guramiko',]
+surnames=['topuria','shavyverashvili','mechechishvili',]
+
+print(names+surnames)
+
+
+#third
+
+
+idk=['zaza',True,False,2,6,"kratos"]
+name="sanchez"
+
+print(len(idk))
+print(len(name))
+
+#forth
+
+some=["tyler",4,"durten","kratos","zeus"]
+some[3]="giga chad"
+print(some[3])
+
+
+# fifth
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+print(numbers[0]+numbers[9])
